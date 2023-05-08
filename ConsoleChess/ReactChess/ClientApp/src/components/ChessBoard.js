e8da35aaae7b8d11637238eb76514e5b53d8770b
LorddTyrion <baczodomonkos@gmail.com>
2023-05-08 14:26:35 +0200

    Timer works on frontend

--- ConsoleChess/ReactChess/ClientApp/src/components/ChessBoard.js
+++ ConsoleChess/ReactChess/ClientApp/src/components/ChessBoard.js
@@ -3,8 +3,20 @@ import { HubConnectionBuilder } from '@microsoft/signalr';
 import authService from './api-authorization/AuthorizeService'
 import { HttpTransportType } from '@microsoft/signalr';
 import { LogLevel } from '@microsoft/signalr';
+import { Clock } from 'chess-clock'
+
 
 import './styles.css';
+
+const fischer = Clock.getConfig('Fischer Rapid 1|5')
+const updateInterval = 500
+const callback = console.info
+
+/*const clock = new Clock({
+  ...fischer,
+  updateInterval,
+  callback,
+})*/
 
 var gameConnection = new HubConnectionBuilder()
     .withUrl('https://localhost:7073/chesshub', {
@@ -22,7 +34,11 @@ export class ChessBoard extends Component {
         super(props);
         this.state = {
             board: [], players: [], loading: true, started: false, joined: false, isWhite: true, turnOf: true, duringMove: false, prevx: 0, prevy: 0, promoteTo: 1, winner: 3,
-            possibleMoves: [], promotionVisible: false
+            possibleMoves: [], promotionVisible: false, clock: new Clock({
+                ...fischer,
+                updateInterval,
+                callback,
+            })
         };
 
         gameConnection.on('AddToGame', (playerlist) => {
@@ -48,7 +64,11 @@ export class ChessBoard extends Component {
             this.setState({ board: board })
             console.log(this.state.board)
             if (!success) console.log("Rossz lépés")
-            else this.setState({ turnOf: !this.state.turnOf })
+            else {
+                if (this.state.turnOf) this.state.clock.push(0);
+                else this.state.clock.push(1);
+                this.setState({ turnOf: !this.state.turnOf })
+            }
             this.forceUpdate()
         })
         gameConnection.on('GetPossibleMoves', (moves) => {
@@ -67,9 +87,39 @@ export class ChessBoard extends Component {
 
         this.startconnection();
     }
+    clockCallback() {
+        console.log("called")
+        if (this.state.clock.state.status === "done") {
+            console.log("DONE")
+        }
+
+    }
 
     componentDidMount() {
+        this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
+        
 
+        this.setState({
+            clock: new Clock({
+                ...fischer,
+                updateInterval,
+                callback: (state)=>{
+                    if(state.status === "done"){
+                        if (state.white === 0) {
+                            console.log("black wins")
+                            this.setState({winner:1})
+                        }
+                        else {
+                            console.log("white wins")
+                            this.setState({winner:0})
+                        }
+                    }
+                }
+            })
+        })
+    }
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
 
     renderJoin(players) {
@@ -101,14 +151,14 @@ export class ChessBoard extends Component {
 
             const cols = [];
             for (let j = 0; j < 8; j++) {
-                var ishighlighted=false;
-                
-                for(let p=0; p<this.state.possibleMoves.length; p++){
-                    if(this.state.possibleMoves[p].targetX===i && this.state.possibleMoves[p].targetY===j){
-                        ishighlighted=true;
+                var ishighlighted = false;
+
+                for (let p = 0; p < this.state.possibleMoves.length; p++) {
+                    if (this.state.possibleMoves[p].targetX === i && this.state.possibleMoves[p].targetY === j) {
+                        ishighlighted = true;
                     }
                 }
-                if(ishighlighted && this.state.turnOf===this.state.isWhite && this.state.duringMove){
+                if (ishighlighted && this.state.turnOf === this.state.isWhite && this.state.duringMove) {
                     cols.push(<td><button className='chess' style={this.returnColor((i + j) % 2 === 0, true)} onClick={() => this.onClick(i, j)}>{this.returnText(i, j)}&#xFE0E;</button></td>);
                 }
                 else {
@@ -134,14 +184,14 @@ export class ChessBoard extends Component {
 
             const cols = [];
             for (let j = 0; j < 8; j++) {
-                var ishighlighted=false;
-                
-                for(let p=0; p<this.state.possibleMoves.length; p++){
-                    if(this.state.possibleMoves[p].targetX===i && this.state.possibleMoves[p].targetY===j){
-                        ishighlighted=true;
+                var ishighlighted = false;
+
+                for (let p = 0; p < this.state.possibleMoves.length; p++) {
+                    if (this.state.possibleMoves[p].targetX === i && this.state.possibleMoves[p].targetY === j) {
+                        ishighlighted = true;
                     }
                 }
-                if(ishighlighted&& this.state.turnOf===this.state.isWhite&& this.state.duringMove){
+                if (ishighlighted && this.state.turnOf === this.state.isWhite && this.state.duringMove) {
                     cols.push(<td><button className='chess' style={this.returnColor((i + j) % 2 === 0, true)} onClick={() => this.onClick(i, j)}>{this.returnText(i, j)}&#xFE0E;</button></td>);
                 }
                 else {
@@ -181,7 +231,7 @@ export class ChessBoard extends Component {
             }
             return { backgroundColor: 'darkgreen', color: 'black', width: '100px', height: '100px', border: 'none', borderRadius: '0px 0px 0px 0px', borderWidth: '0px', textAlign: 'center', bottom: '0', fontSize: '80px' };
         }
-        else{
+        else {
             if (white === true) {
                 return { backgroundColor: 'lightgrey', color: 'black', width: '100px', height: '100px', border: 'none', borderRadius: '0px 0px 0px 0px', borderWidth: '0px', textAlign: 'center', bottom: '0', fontSize: '80px' };
             }
@@ -242,6 +292,9 @@ export class ChessBoard extends Component {
     }
 
     render() {
+        if (this.state.clock.state.status === "done") {
+            
+        }
         let join = this.state.started ? <div></div> : this.renderJoin(this.state.players)
         console.log(this.state.possibleMoves)
         let content = <div></div>
@@ -251,7 +304,7 @@ export class ChessBoard extends Component {
         else {
             content = this.state.loading ? <div></div> : this.renderBlack()
         }
-        let promote = !this.state.promotionVisible?<div></div>:this.renderPromoteSelection()
+        let promote = !this.state.promotionVisible ? <div></div> : this.renderPromoteSelection()
         let win = <div></div>
 
         switch (this.state.winner) {
@@ -268,15 +321,23 @@ export class ChessBoard extends Component {
                 break;
 
         }
+        let whiteminutes = Math.floor(this.state.clock.state.remainingTime[0] / 60000).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+        let blackminutes = Math.floor(this.state.clock.state.remainingTime[1] / 60000).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+        let whiteseconds = Math.floor((this.state.clock.state.remainingTime[0] % 60000) / 1000).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+        let blackseconds = Math.floor((this.state.clock.state.remainingTime[1] % 60000) / 1000).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
         let all = !this.state.started ? <div>{join}</div> : <div>
             {content}
             {promote}
+            <p aria-live="polite">White time: <strong>{whiteminutes}:{whiteseconds}</strong></p>
+            <p aria-live="polite">Black time: <strong>{blackminutes}:{blackseconds}</strong></p>
         </div>
         if (this.state.winner <= 2) all = win
+
 
         return (
             <div>
                 {all}
+
             </div>
 
         );
@@ -300,12 +361,12 @@ export class ChessBoard extends Component {
         if (this.state.turnOf !== this.state.isWhite) return
         if (!this.state.duringMove) {
             this.setState({ duringMove: true, prevx: x, prevy: y })
-            if(this.state.isWhite && x===6 && this.state.board[8 * x + y].piece.pieceName===5 && this.state.board[8 * x + y].piece.isWhite) this.setState({promotionVisible: true})
-            else if(!this.state.isWhite && x===1 && this.state.board[8 * x + y].piece.pieceName===5 && !this.state.board[8 * x + y].piece.isWhite) this.setState({promotionVisible: true})
+            if (this.state.isWhite && x === 6 && this.state.board[8 * x + y].piece.pieceName === 5 && this.state.board[8 * x + y].piece.isWhite) this.setState({ promotionVisible: true })
+            else if (!this.state.isWhite && x === 1 && this.state.board[8 * x + y].piece.pieceName === 5 && !this.state.board[8 * x + y].piece.isWhite) this.setState({ promotionVisible: true })
             await gameConnection.invoke('PossibleMoves', x, y);
         }
         else {
-            this.setState({ duringMove: false, possibleMoves: [], promotionVisible:false });
+            this.setState({ duringMove: false, possibleMoves: [], promotionVisible: false });
             await gameConnection.invoke('MakeMove', this.state.prevx, this.state.prevy, x, y, this.state.promoteTo);
         }
     }
